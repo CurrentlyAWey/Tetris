ef9d2e386cfe22c9bb1dd9175451bc2986d7dc77
PandaStrawberry <wey@cooper.edu>
2015-04-12 23:48:37 -0400

    Formatted etc

--- shapeTest.c
+++ shapeTest.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "tetris.h"
 #include "BoolStr.h"
-#include <stdbool.h>
 
-//TODO: 
+
+//TODO:
 //		Load from input file?
 
 
-main(){
+main() {
 
 	FILE *ptr_file;
 	char buf[1000];
 
 	ptr_file = fopen("input.bin", "r");
 
-	if(!ptr_file){
+	if (!ptr_file) {
 		printf("what\n");
 		return 1;
 	}
 
 	shape swag[7];
-	
+
 	swag[0].name = 'I';
 	swag[1].name = 'J';
 	swag[2].name = 'L';
@@ -28,7 +29,7 @@ main(){
 	swag[4].name = 'S';
 	swag[5].name = 'T';
 	swag[6].name = 'O';
-	
+
 	for (int i = 0; i < 7; ++i)
 	{
 		swag[i].orientation = 4;
@@ -37,17 +38,15 @@ main(){
 
 	}
 
-	
 	//initialize all arrays
-	
-int i = 0;
+	int i = 0;
 	for (int i = 0; i < 7; ++i) //each shape
 	{
 
-	putchar(swag[i].name);
-	for (int orientation = 0; orientation < swag[i].orientation; ++orientation) // each orientation
-	{
-		switch (orientation){
+		putchar(swag[i].name);
+		for (int orientation = 0; orientation < swag[i].orientation; ++orientation) // each orientation
+		{
+			switch (orientation) {
 			case 0:
 				printf("up\n");
 				break;
@@ -60,35 +59,35 @@ int i = 0;
 			case 3:
 				printf("left\n");
 				break;
-		}
-		for (int row = 0; row < swag[i].row; ++row) // each row
-		{
-			int num;
-			char string[8];
-			scanf("%s", string);
-			for(int col = 0; col < swag[i].col; ++col)	//TODO :scan in keyboard input (8 things of 1's and 0's, 
+			}
+			for (int row = 0; row < swag[i].row; ++row) // each row
 			{
-				if ((string[col] == 49) & (col<4))
+				int num;
+				char string[8];
+				scanf("%s", string);
+				for (int col = 0; col < swag[i].col; ++col)	//TODO :scan in keyboard input (8 things of 1's and 0's,
 				{
-					swag[i].up[orientation][row][col] = 1;		
+					if ((string[col] == 49) & (col < 4))
+					{
+						swag[i].up[orientation][row][col] = 1;
+					}
+					else
+					{
+						swag[i].up[orientation][row][col] = 0;
+					}	//conv. to array of 8 booleans and assign to swag[i].up[orientation][row])
 				}
-				else 
-				{
-					swag[i].up[orientation][row][col] = 0;
-				}	//conv. to array of 8 booleans and assign to swag[i].up[orientation][row])
 			}
 		}
-}
 	}
 
-for (int sh = 0; sh < 7; ++sh)
+	for (int sh = 0; sh < 7; ++sh)
 	{
 		p3bs(swag[sh].up);
 		for (int i = 0; i < 7; ++i)
 		{
 			s3bs(swag[sh].up);
 			p3bs(swag[sh].up);
-			
+
 		}
 	}
 	//print out all the shapes (kms)
--- tetris.h
+++ tetris.h
@@ -1,17 +1,19 @@
-void showbits(unsigned char x){
+#include <stdio.h>
+
+void showbits(unsigned char x) {
 	int i;
-	for(i=(sizeof(unsigned char)*8)-1; i>=0; i--){
-        (x&(1<<i))?putchar('1'):putchar('0');
-    }
+	for (i = (sizeof(unsigned char) * 8) - 1; i >= 0; i--) {
+		(x & (1 << i)) ? putchar('1') : putchar('0');
+	}
 	printf("\n");
 }
 
-unsigned char RotateRight(unsigned char n, unsigned char i){
-	return (n >> i) | (n << (sizeof(unsigned char)*8 - i));
+unsigned char RotateRight(unsigned char n, unsigned char i) {
+	return (n >> i) | (n << (sizeof(unsigned char) * 8 - i));
 }
 
-unsigned char RotateLeft(unsigned char n, unsigned char i){
-	return (n << i) | (n >> (sizeof(unsigned char)*8 -i));
+unsigned char RotateLeft(unsigned char n, unsigned char i) {
+	return (n << i) | (n >> (sizeof(unsigned char) * 8 - i));
 }
 
 
