3f8c5ad84f3c11651161c7c2362bf3f2c6dbfb02
PandaStrawberry <wey@cooper.edu>
2015-04-14 14:51:32 -0400

    Organization

---
+++ Tetromino Generator/BoolStr.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void p1bs(bool arr[]) {
+	for (int i = 0; i < 8; ++i)
+	{
+		if (arr[i])
+			putchar('1');
+		else
+			putchar('0');
+	}
+	putchar('\n');
+}
+
+void p2bs(bool arr[][8]) {
+	//print 2D Bitstring
+	for (int row = 0; row < 4; ++row)
+		p1bs(arr[row]);
+	putchar('\n');
+}
+
+void p3bs(bool arr[][4][8]) {
+	for (int i = 0; i < 4; ++i)
+		p2bs(arr[i]);
+}
+
+void s2bs(bool arr[][8]) {
+	for (int i = 0; i < 4; ++i)
+	{
+		bool hold = arr[i][7];
+		for (int x = 6; x >= 0; --x)
+			arr[i][(x + 1)] = arr[i][x];
+		arr[i][0] = hold;
+	}
+}
+
+void s3bs(bool arr[][4][8]) {
+	for (int i = 0; i < 4; ++i)
+		s2bs(arr[i]);
+}
---
+++ Tetromino Generator/shiftRight.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "tetris.h"
+#include "BoolStr.h"
+
+/* Playground */
+int main(int argc, char const *argv[]) {
+    int j = 5225, m, n;
+    printf("The decimal %d is equal to binary - ", j);
+    showbits(j);
+
+    bool swag[8] = {0, 1, 1, 0, 1, 0, 0, 1};
+    bool test[4][4][8] = {
+        {
+            {1, 1, 1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        },
+        {
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        },
+        {
+            {1, 1, 1, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0}
+        },
+        {
+            {0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 0}
+        }
+    };
+
+    //print 3d BoolString
+    p3bs(test);
+    putchar('\n');
+
+    //shifts each row in 4x8 array to the right 1 element
+    s3bs(test);
+    p3bs(test);
+
+
+    // Rotate Right, No Carry
+    for (int i = 0; i < 4; ++i) {
+        int r = RotateRight(j, i);
+        printf("%d Rotated Right %d is ", j, i);
+        showbits(r);
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        int r = RotateLeft(j, i);
+        printf("%d Rotated Left %d is ", j, i);
+        showbits(r);
+    }
+
+    return 0;
+
+
+}
